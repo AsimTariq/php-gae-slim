2c84dda44d6769da7ac40beff5bdcd0e88e7fd92
Michael Johansen <michael@redperformance.no>
2018-03-27 18:36:38 +0200

    initial commit

---
+++ src/App.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michael
+ * Date: 23/03/2018
+ * Time: 01:47
+ */
+
+namespace GaeSlim;
+
+use GaeUtil\Util;
+use Slim\Container;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class App extends \Slim\App {
+
+    public function __construct($container = null) {
+        $container = new Container([
+            'settings' => [
+                'displayErrorDetails' => Util::isDevServer(),
+            ],
+        ]);
+
+        /**
+         * Setting default Respons header to text/plain
+         */
+        $this->add(function (Request $request, Response $response, $next) {
+            $newResponse = $response->withHeader('Content-type', 'text/plain;charset=utf-8');
+            return $next($request, $newResponse);
+        });
+
+        parent::__construct($container);
+    }
+
+}
+
