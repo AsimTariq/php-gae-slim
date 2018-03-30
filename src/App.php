0633c587bfc2313dcbcfd51d7c0b6243cd2011e6
Michael Johansen <michael@redperformance.no>
2018-03-31 01:11:07 +0200

    added functions to remove trailing slashes

--- src/App.php
+++ src/App.php
@@ -24,7 +24,7 @@ class App extends \Slim\App {
 
         /**
          * Setting default Respons header to text/plain
-*/
+         */
         $this->add(function (Request $request, Response $response, $next) {
             $newResponse = $response->withHeader('Content-type', 'text/plain;charset=utf-8');
             return $next($request, $newResponse);
@@ -33,5 +33,22 @@ class App extends \Slim\App {
         parent::__construct($container);
     }
 
+    public function removeTrailingSlashes() {
+        $this->get('/{path:.*}/', function (Request $request, Response $response) {
+            $path = $request->getUri()->getPath();
+            $new_path = rtrim($path, "/");
+            $new_uri = $request->getUri()->withPath($new_path);
+            if ($new_uri->getPort() == 80) {
+                $new_uri = $new_uri->withPort(null);
+            }
+            return $response->withRedirect($new_uri);
+        });
+    }
+
+    public function simpleRedirect($from, $to) {
+        $this->get($from, function (Request $request, Response $response) use ($to) {
+            return $response->withRedirect($to);
+        });
+    }
 }
 
